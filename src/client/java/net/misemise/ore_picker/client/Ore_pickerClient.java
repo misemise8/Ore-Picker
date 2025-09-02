131092ed337ec920361e871c1ccc36a7d8aa564b
misemise8 <semi.twitter42@gmail.com>
2025-09-02 20:14:55 +0900

    Just Added

---
+++ src/client/java/net/misemise/ore_picker/client/Ore_pickerClient.java
@@ -0,0 +1,10 @@
+package net.misemise.ore_picker.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Ore_pickerClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/misemise/ore_picker/Ore_picker.java
@@ -0,0 +1,10 @@
+package net.misemise.ore_picker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ore_picker implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
